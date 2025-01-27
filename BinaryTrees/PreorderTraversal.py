cad5abcf8afd7449f8ba335ebced1a48fac7aff6
Manish Gupta <manish15gupta03@gmail.com>
2025-01-27 22:02:20 +0530

    Preorder Traversal

---
+++ BinaryTrees/PreorderTraversal.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+# Question:
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+# Logic: Root-Left-Right
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorder(self, node, arr):
+        if not node:
+            return
+        arr.append(node.val)
+        self.preorder(node.left, arr)
+        self.preorder(node.right, arr)
+
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        answer = []
+        self.preorder(root, answer)
+        return answer
