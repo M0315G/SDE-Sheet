61b3140a167d8b1c9edea19ca715eee794387ea6
Manish Gupta <manish15gupta03@gmail.com>
2025-02-13 21:52:56 +0530

    Assign Cookies

---
+++ Greedy/AssignCookies.py
@@ -0,0 +1,21 @@
+from typing import List
+
+# Question
+# Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most one cookie.
+# Each child i has a greed factor g[i], which is the minimum size of a cookie that the child will be content with; and each cookie j
+# has a size s[j]. If s[j] >= g[i], we can assign the cookie j to the child i, and the child i will be content. Your goal is to maximize
+# the number of your content children and output the maximum number.
+
+# Logic:
+# Sort both arrays and iterate using 2 pointers
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g = sorted(g)
+        s = sorted(s)
+        l, r = 0, 0
+        while l < len(s) and r < len(g):
+            if g[r] <= s[l]:
+                r += 1
+            l += 1
+        return r
