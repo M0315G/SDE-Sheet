35465a2b9b85c8408aceaab38ed0a4d5b06eda17
Manish Gupta <manish15gupta03@gmail.com>
2025-03-03 09:27:58 +0530

    Partition Array According to given pivot

---
+++ TwoPointers/PartitionArrayAccordingToGivenPivot.py
@@ -0,0 +1,30 @@
+from typing import List
+
+# Question:
+# You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+# Every element less than pivot appears before every element greater than pivot.
+# Every element equal to pivot appears in between the elements less than and greater than pivot.
+# The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+# More formally, consider every pi, pj where pi is the new position of the ith element and pj is the new position of the jth element. If i < j and both elements are smaller (or larger) than pivot, then pi < pj.
+# Return nums after the rearrangement
+
+# Logic:
+# We use the same low, mid and high approach as used in the "FruitsIntoBasket" problem, just this time we dont swap mid and high, rather append it in the end
+# and then pop the mid to maintain the overall order.
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        low, mid = 0, 0
+        high = len(nums) - 1
+        while mid <= high:
+            if nums[mid] < pivot:
+                nums[mid], nums[low] = nums[low], nums[mid]
+                low += 1
+                mid += 1
+            elif nums[mid] == pivot:
+                mid += 1
+            else:
+                nums.append(nums[mid])
+                nums.pop(mid)
+                high -= 1
+        return nums
